ed2081f94de71303dc188df13b0a5d6dd1de4a7e
Nicolas Frankel <nicolas@frankel.ch>
2020-09-08 17:26:42 +0200

    Generate native image

--- src/main/java/ch/frankel/blog/springindocker/SpringInDockerApplication.java
+++ src/main/java/ch/frankel/blog/springindocker/SpringInDockerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods=false)
 public class SpringInDockerApplication {
 
     public static void main(String[] args) {
